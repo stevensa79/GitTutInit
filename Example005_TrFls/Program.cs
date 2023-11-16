544385dc2c074497c8458ae56d978babd0fe0fc0
Steven <xscorpion999x@gmail.com>
2023-11-16 22:16:53 +0400

    add Name Console

---
+++ Example005_TrFls/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Your Name ");
+string username = Console.ReadLine();
+if (username.ToLower() == "john")
+{         
+        Console.Write("Hello Jhone, your fate is in Skynet hands");
+ }
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
